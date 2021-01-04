6620ee28b1cd778a9b2fa6749a7266f8ad7f932e
psbin2017 <33011631+psbin2017@users.noreply.github.com>
2021-01-04 16:17:07 +0900

    [INIT] jpa project
    Language : Java 1.8
    Build Tool : Maven
    Spring Boot : 2.4.1

---
+++ jpa/src/main/java/com/practice/jpa/PracticeSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringJpaApplication.class, args);
+	}
+
+}
