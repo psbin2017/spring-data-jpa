6501f9accb0abcfb29ae42605374a5dee5f729e3
psbin2017 <123qkrtnqls@gmail.com>
2021-03-09 09:09:54 +0900

    [MOD] @EnableJpaAuditing

---
+++ self-webservice/src/main/java/com/self/webservice/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.self.webservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
