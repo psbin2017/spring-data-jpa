591f758e3c556d1d7c5cd6f3bb0d28941e4abc1b
psbin2017 <123qkrtnqls@gmail.com>
2021-03-23 18:37:34 +0900

    ✏ 상속 관계 구조
    주석 추가

--- jpa/src/main/java/com/practice/jpa/domain/item/domain/Item.java
+++ jpa/src/main/java/com/practice/jpa/domain/item/domain/Item.java
@@ -5,6 +5,20 @@ import lombok.*;
 
 import javax.persistence.*;
 
+/**
+ * 상속 관계 구조
+ *
+ * - `InheritanceType.JOIN`
+ *  부모 테이블과 자식 테이블이 별도로 생성 됨
+ *
+ * - `InheritanceType.SINGLE_TABLE`
+ *  부모 테이블만 생성 됨 (정규화되지 않음)
+ *
+ * - `InheritanceType.TABLE_PER_CLASS`
+ *  구현 클래스만 생성됨 (부모 클래스는 추상으로 선언되어 있음을 주목)
+ *
+ * 예) 공통된 컬럼을 가지면서 (abstract class) 부가적인 고유 컬럼을 가진 형태
+ */
 @Entity
 @Table(name = "ITEM")
 @Inheritance(strategy = InheritanceType.JOINED)
